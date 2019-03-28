78f2636862d098e8b9f4ef4bfe8d6aed9ae6b6b9
vagrant <vagrant@homestead>
2019-03-28 01:13:22 +0000

    + Pass data from controller to view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use Auth;
+use Gate;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::published()->paginate();
+        return view('welcome', compact('posts'));
+    }
+
 }
